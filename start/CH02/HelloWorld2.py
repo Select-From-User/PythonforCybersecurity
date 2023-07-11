4b550872117efe98f86ed8028b9b5c624eb1fd81
select-from-user <cory.m.alonso@gmail.com>
2023-07-11 00:43:38 -0700

    started Helloworld and HW2

--- start/CH02/HelloWorld2.py
+++ start/CH02/HelloWorld2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python with Inputs
-# Created 
+# Created by cory on jul 10
 
-# Suggestion, build out 1 line at a time
-# Once multiple print statemetns exist, put a breakpoint at first print line
-# Then walk through as an example of "debugging"
+#ask user for their name
+user_name = input("What is your name? ")
+
+#print hello and their name
+print("Hello {0}!".format(user_name))
