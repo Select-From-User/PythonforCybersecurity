69b27d551935bfe3335f89e027e496f249d15f0f
select-from-user <cory.m.alonso@gmail.com>
2023-07-11 01:06:27 -0700

    fixed

--- start/CH02/HelloWorld3.py
+++ start/CH02/HelloWorld3.py
@@ -7,8 +7,8 @@ user_name = input("What is your name? ")
 
 #print hello and their name
 print("Hello {0}!".format(user_name))
-print(f"hello {user_name}")
-print("Hello" + user_name)
+print(f"Hello {user_name}")
+print("Hello " + user_name)
 print("Hello", user_name)
 message = "Hello" + user_name
 print(message)
