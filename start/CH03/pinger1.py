29292395232b17f64b234bcae6db4ba260996cbf
select-from-user <cory.m.alonso@gmail.com>
2023-07-11 03:01:44 -0700

    complete

--- start/CH03/pinger1.py
+++ start/CH03/pinger1.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
 # First example of pinging from Python
-# By 
+# By cory on jul 11
+#
+
+import os
+
+#assign ip to variable  
+ip_address = "8.8.8.8"
+#build our ping command
+ping_cmd = f"ping -c 1 -w 1 {ip_address} > /dev/null 2>&1"
+#execute the ping command
+exit_code = os.system(ping_cmd)
+#print out results
+print(exit_code)
