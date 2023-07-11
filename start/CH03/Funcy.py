f74e7fc8bafdca3c6e5d2a5fa7956225826b8339
select-from-user <cory.m.alonso@gmail.com>
2023-07-11 04:47:54 -0700

    new func

--- start/CH03/Funcy.py
+++ start/CH03/Funcy.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
 # example workign with Functions
-#By 
+#By cory on jul 11
+
+#my function
+def print_me(my_message):
+    print(my_message)
+    return "it worked"
+
+def say_hello(num_times):
+        for x in range(num_times):
+            print("hello world")
+
+#calling the function
+print_me("this is a function")
+result = print_me("this is another funtion with return value")
+print(result)
+say_hello(3)
