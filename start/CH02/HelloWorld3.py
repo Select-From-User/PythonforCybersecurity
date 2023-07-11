8826db9cc244dec040fd11945eaeac678381bc7c
select-from-user <cory.m.alonso@gmail.com>
2023-07-11 01:01:23 -0700

    multiple options

--- start/CH02/HelloWorld3.py
+++ start/CH02/HelloWorld3.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
-# A more complex "Hello World" script in python with Inputs
-# Created 
+# A simple "Hello World" script in python with Inputs
+# Created by cory on jul 10
+
+#ask user for their name
+user_name = input("What is your name? ")
+
+#print hello and their name
+print("Hello {0}!".format(user_name))
+print(f"hello {user_name}")
+print("Hello" + user_name)
+print("Hello", user_name)
+message = "Hello" + user_name
+print(message)
