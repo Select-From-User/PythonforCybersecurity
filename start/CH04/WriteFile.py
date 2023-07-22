fc4e350e9210580eab039f8d4bacd16188e83eb6
select-from-user <cory.m.alonso@gmail.com>
2023-07-22 15:50:42 -0700

    simple write file

--- start/CH04/WriteFile.py
+++ start/CH04/WriteFile.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
 # Sample script that writes to a file
-# By 
+# By cory 7/22/23
+
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+#open file for writing
+
+f = open(dir_path + "/testfile.txt", "w")
+
+#write to the file
+print("hello world")
+f.write("hello world\n")
+f.write("hello world\n")
+f.write("hello world\n")
+#close the file
+f.close()
+
